1c39561f8ef045b9d4cbf5d9080bc35a7eabe7ca
Arjun Mitra <mitraarj@usc.edu>
2021-03-02 17:26:33 +0530

    added StudentActivity class
    added Student Activity class. Unsure of whether we should dd a set checkOutTime method

---
+++ android/app/src/main/java/com/example/app/StudentActivity.java
@@ -0,0 +1,36 @@
+package com.example.app;
+
+import java.time.LocalDateTime;
+
+public class StudentActivity {
+    private String buildingName;
+    private LocalDateTime checkInTime;
+    private LocalDateTime checkOutTime;
+
+    public StudentActivity(String name, LocalDateTime in, LocalDateTime out){
+        buildingName=name;
+        checkInTime=in;
+        out = out;
+    }
+    public StudentActivity(String name, LocalDateTime in){
+        buildingName=name;
+        checkInTime=in;
+        checkOutTime=null;
+    }
+
+    public String getBuildingName(){
+        return buildingName;
+    }
+
+    public LocalDateTime getCheckInTime(){
+        return checkInTime;
+    }
+
+    public LocalDateTime getCheckOutTime(){
+        return checkOutTime;
+    }
+
+    public void setCheckOutTime(LocalDateTime checkOutTime) {
+        this.checkOutTime = checkOutTime; // do we need to edit checkout ?
+    }
+}
