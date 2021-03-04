d641e6bc21d65798804c493f0aeb015589f3c828
Hassib Rangeen <hassibrangeen@Hassibs-MacBook-Pro-M1.local>
2021-03-03 20:23:24 -0800

    fixed gradle file to reflect sdk 10

--- android/app/src/androidTest/java/com/example/app/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/example/app/ExampleInstrumentedTest.java
@@ -8,6 +8,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.naming.Context;
+
 import static org.junit.Assert.*;
 
 /**
