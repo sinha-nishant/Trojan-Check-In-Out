81609cbae3d08b23b22120080ade2804261d98db
Hassib Rangeen <hassibrangeen@Hassibs-MacBook-Pro-M1.local>
2021-03-04 19:37:19 -0800

    created libs for jar files and started Loginout

--- android/app/src/main/java/com/example/app/HassibTest.java
+++ android/app/src/main/java/com/example/app/HassibTest.java
@@ -3,12 +3,41 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class HassibTest extends AppCompatActivity {
+    private EditText eEmail;
+    private EditText ePassword;
+    private Button eLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hassib_test);
+
+        eEmail = findViewById(R.id.etEmail);
+        ePassword = findViewById(R.id.etPassword);
+        eLogin = findViewById(R.id.btnLogin);
+        eLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String inputEmail = eEmail.getText().toString();
+                String inputPass = ePassword.getText().toString();
+                if(inputEmail.isEmpty() || inputPass.isEmpty())
+                {
+                    Toast.makeText(HassibTest.this, "Enter all fields with text", Toast.LENGTH_SHORT).show();
+                }else{
+                    LogInOut login = new LogInOut();
+                    boolean success_login=login.LogIn(inputEmail,inputPass);
+                }
+
+            }
+        });
+
     }
 }
---
+++ android/app/src/main/java/com/example/app/LogInOut.java
@@ -0,0 +1,22 @@
+package com.example.app;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+public class LogInOut {
+    public LogInOut(){
+
+    }
+    public boolean LogIn(String email, String Password){
+        String password = "1234";
+        String bcryptHashString = BCrypt.withDefaults().hashToString(12, password.toCharArray());
+
+
+        BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), bcryptHashString);
+
+// result.verified == true
+        return true;
+    }
+    public void LogOut(){
+
+    }
+}
