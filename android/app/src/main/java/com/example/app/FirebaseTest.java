e6d50571a62915e39ebaee3e22a14f7fd663ef3e
Nishant Sinha <nishant813@gmail.com>
2021-03-05 01:22:47 -0800

    Completed checkIn

--- android/app/src/main/java/com/example/app/FirebaseTest.java
+++ android/app/src/main/java/com/example/app/FirebaseTest.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +23,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -29,7 +31,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 public class FirebaseTest extends AppCompatActivity {
 
-    private CollectionReference accounts = FirebaseFirestore.getInstance().collection("Accounts");
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +41,46 @@ public class FirebaseTest extends AppCompatActivity {
 
     // FOR TESTING PURPOSES
     public void test(View v) {
-        EditText textInput = findViewById(R.id.textInput);
-        String text = textInput.getText().toString();
-        checkEmailExists(text);
+//        EditText textInput = findViewById(R.id.textInput);
+//        String text = textInput.getText().toString();
+
+        StudentActivity sa = new StudentActivity("Leventhal School of Accounting", LocalDateTime.now());
+        checkIn(42, sa);
     }
 
     // CheckInOut
     public void checkIn (int uscID, StudentActivity sa) {
+        CollectionReference accounts = db.collection("Accounts");
+        Query studentQuery = accounts.whereEqualTo("uscID", uscID);
+        studentQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if (!task.getResult().isEmpty()) {
+                        for (QueryDocumentSnapshot document: task.getResult()) {
+                            accounts.document(document.getId()).update("activity", FieldValue.arrayUnion(sa));
+                        }
 
+                        CollectionReference buildings = db.collection("Buildings");
+                        Query buildingQuery = buildings.whereEqualTo("name", sa.getBuildingName());
+                        buildingQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (!task.getResult().isEmpty()) {
+                                        for (QueryDocumentSnapshot document: task.getResult()) {
+                                            buildings.document(document.getId()).update("students", FieldValue.arrayUnion(uscID));
+                                        }
+
+                                        // call callback function
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
     }
 
     public void checkOut(int uscID, String buildingName, LocalDateTime checkOutTime) {
@@ -61,6 +95,7 @@ public class FirebaseTest extends AppCompatActivity {
     // WORKS
     public Boolean checkEmailExists(String email) {
         Boolean exists = false;
+        CollectionReference accounts = db.collection("Accounts");
         Query query = accounts.whereEqualTo("email", email);
         query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
