b029525337bbde058bf3495d751acba60384b8b3
Hassib Rangeen <hassibrangeen@Hassibs-MacBook-Pro-M1.local>
2021-03-06 13:02:19 -0800

    added stuff to building

--- android/app/src/main/java/com/example/app/Building.java
+++ android/app/src/main/java/com/example/app/Building.java
@@ -20,30 +20,21 @@ public class Building {
     private List<Integer> students;//list of uscId
     private String qrCodeURL;
 
-    public Building(String building_name, Integer max_capacity, Integer current_occupancy,List<Integer> students_in_building){
+    public Building(String building_name, Integer max_capacity, Integer current_occupancy,String image_location ,List<Integer> students_in_building){
         name=building_name;
         capacity=max_capacity;
         occupancy=current_occupancy;
+        qrCodeURL=image_location;
         if(students_in_building!=null){
             students=students_in_building;
         }
         else{
             students = new ArrayList<Integer>();
         }
-
-
-
-
     }
-    public boolean setCapacity(int newCapacity){
-        return true;
-    }
-    public boolean checkIn(StudentAccount student){
-        return true;
-    }
-    public boolean checkOut(StudentAccount student){
-        return true;
-    }
+    public Building(){}
+
+    //getters
     public String getName(){
         return name;
     }
@@ -53,15 +44,42 @@ public class Building {
     public Integer getOccupancy(){
         return occupancy;
     }
-    public List<StudentAccount> getStudents(){
-        List<StudentAccount> students_from_firebase = new ArrayList<StudentAccount>();
-        if(!students.isEmpty()){//if we have list of ids then extract students from firebase
-           students_from_firebase = FirebaseTest.getStudents(students);
-        }
-        return students_from_firebase;
-    }
     public String getQrCodeURL(){
         return qrCodeURL;
     }
+    public List<StudentAccount> getStudents(){
+        List<StudentAccount> students_from_firebase = new ArrayList<StudentAccount>();
+        if(!students.isEmpty()){//if we have list of ids then extract students from firebase
+            students_from_firebase = FirebaseTest.getStudents(students);
+        }
+        return students_from_firebase;
+    }
+    //setters
+    public void setName(String new_name){
+        name=new_name;
+    }
+    public void setCapacity(int new_capacity){
+        capacity=new_capacity;
+    }
+    public void setOccupancy(int new_occ){
+        occupancy=new_occ;
+    }
+    public void setQrCodeURL(String url){
+        qrCodeURL=url;
+    }
+    public void setStudents(List<Integer> student_ids){
+        students=student_ids;
+    }
+
+
+    public boolean checkIn(StudentAccount student){
+        return true;
+    }
+    public boolean checkOut(StudentAccount student){
+        return true;
+    }
+
+
+
 
 }
