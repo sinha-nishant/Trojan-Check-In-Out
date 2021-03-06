b39a4f46e6519a96137cfb2f91fddc716574fde8
Nishant Sinha <nishant813@gmail.com>
2021-03-05 21:43:16 -0800

    Fixed bcrypt, didnt amend email hardcode

--- android/app/src/main/java/com/example/app/LogInOut.java
+++ android/app/src/main/java/com/example/app/LogInOut.java
@@ -1,18 +1,19 @@
 package com.example.app;
 
 
+import android.util.Log;
+
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
 public class LogInOut {
     public LogInOut(){
 
     }
-    public boolean LogIn(String email, String Password){
-        String password = "1234";
+    public boolean LogIn(String email, String password){
         String bcryptHashString = BCrypt.withDefaults().hashToString(12, password.toCharArray());
-
-
+        Log.d("TEST", "Hashed: " + bcryptHashString);
         BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), bcryptHashString);
+        Log.d("TEST", result.toString());
 
 // result.verified == true
         return true;
