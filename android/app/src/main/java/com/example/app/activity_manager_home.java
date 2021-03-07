c4b871427e283612ff179152d56fa2734d97c0e0
Markus Snelder <snelder@usc.edu>
2021-03-06 19:24:17 -0800

    UI Updates
    From Markus

---
+++ android/app/src/main/java/com/example/app/activity_manager_home.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_manager_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_home);
+    }
+}
