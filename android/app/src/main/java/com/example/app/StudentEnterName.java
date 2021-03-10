21fd8fe88597f4cb41f63f084dabcf71f49ed6d6
John Paine <johnpain@usc.edu>
2021-03-09 16:50:42 -0800

    Student signup ready for backend/database processing

--- android/app/src/main/java/com/example/app/StudentEnterName.java
+++ android/app/src/main/java/com/example/app/StudentEnterName.java
@@ -25,10 +25,11 @@ public class StudentEnterName extends AppCompatActivity {
         lastNameInput = (EditText) findViewById(R.id.studentLastName);
 
         //Get the bundle
-        // Bundle bundle = getIntent().getExtras();
+        Bundle bundle = getIntent().getExtras();
 
         //Extract the data…
-        // String stuff = bundle.getString(“stuff”);
+        String email = bundle.getString("email");
+        String password = bundle.getString("password");
 
         submitButton = (Button) findViewById(R.id.nameButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -40,19 +41,15 @@ public class StudentEnterName extends AppCompatActivity {
                 //used for popups to user
                 //showToast(email);
                 //showToast(password);
-                /*
-                if(email.length() == 0 || password.length() == 0)
-                    showToast("Email or Password is blank");
-                else if(email.length() < 8 || !email.substring(email.length()-8).equals("@usc.edu"))
-                    showToast("Invalid Email");
+                if(fName.length() == 0 || lName.length() == 0)
+                    showToast("First or last name is blank");
                 else
                 {
                     //don't put info in database yet, keep passing values until completed
                     //go to next activity
-                    openNameStudent(email, password);
+                    showToast(email);
+                    openID(email, password, fName, lName);
                 }
-                */
-                openID("email", "pass");
             }
         });
 
@@ -61,11 +58,13 @@ public class StudentEnterName extends AppCompatActivity {
     {
         Toast.makeText(StudentEnterName.this, text, Toast.LENGTH_SHORT).show();
     }
-    public void openID(String email, String password) {
+    public void openID(String email, String password, String fName, String lName) {
         Intent i = new Intent(this, StudentEnterID.class);
         Bundle bundle = new Bundle();
         bundle.putString("email", email);
         bundle.putString("password", password);
+        bundle.putString("fName", fName);
+        bundle.putString("lName", lName);
         i.putExtras(bundle);
         startActivity(i);
     }
--- android/app/src/main/java/com/example/app/StudentUploadPhoto.java
+++ android/app/src/main/java/com/example/app/StudentUploadPhoto.java
@@ -9,8 +9,11 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class StudentUploadPhoto extends AppCompatActivity {
+
+    String email, password, fName, lName, id, major;
 
     ImageView uploadImage;
     Button bUploadImage;
@@ -22,13 +25,37 @@ public class StudentUploadPhoto extends AppCompatActivity {
         setContentView(R.layout.activity_student_upload_photo);
 
         uploadImage = (ImageView)findViewById(R.id.imageToUpload);
-        bUploadImage = (Button)findViewById(R.id.uploadImageButton);
+        //To Backend: What data y'all get:
+        Bundle bundle = getIntent().getExtras();
+
+        email = bundle.getString("email");
+        password = bundle.getString("password");
+        fName = bundle.getString("fName");
+        lName = bundle.getString("lName");
+        id = bundle.getString("id");
+        major = bundle.getString("major");
+
+        showToast(fName + " " + lName + " (" + id + ")\n"
+                + major + " " + email + "\nPass: " + password);
+        //End data
 
         uploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        bUploadImage = (Button)findViewById(R.id.uploadImageButton);
+        bUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //BACKEND: probably here is where you want to upload to database, wait until upload
+                //is complete
+                showToast("Success!");
+                //go back to John's page, would normally be profile
+                openProfile();
             }
         });
     }
@@ -43,4 +70,21 @@ public class StudentUploadPhoto extends AppCompatActivity {
             uploadImage.setImageURI(selectedImage);
         }
     }
+
+    private void showToast(String text)
+    {
+        Toast.makeText(StudentUploadPhoto.this, text, Toast.LENGTH_LONG).show();
+    }
+
+    public void openProfile() {
+        //What unique identifier will be used to draw up profile page? Email?
+        Intent i = new Intent(this, JohnTest.class);
+        /*
+        Bundle bundle = new Bundle();
+        bundle.putString("email", email);
+        i.putExtras(bundle);
+         */
+        startActivity(i);
+    }
 }
+
