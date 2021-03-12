2e7243161c08c6a4150ed49be36afc13e1bae79f
Markus Snelder <snelder@usc.edu>
2021-03-12 00:00:21 -0800

    wmqf

---
+++ android/app/src/main/java/com/example/app/ManagerHomeEdit.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerHomeEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_home_edit);
+    }
+}
