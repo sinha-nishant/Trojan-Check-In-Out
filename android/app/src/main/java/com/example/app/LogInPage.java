28e053ab4cf072742740cf3c20bebfe1876afcec
Arjun Mitra <mitraarj@usc.edu>
2021-03-14 18:27:39 +0530

    added processing for login and added progress bar

--- android/app/src/main/java/com/example/app/LogInPage.java
+++ android/app/src/main/java/com/example/app/LogInPage.java
@@ -1,12 +1,21 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import javax.annotation.Nullable;
 
 public class LogInPage extends AppCompatActivity {
 
@@ -14,6 +23,10 @@ public class LogInPage extends AppCompatActivity {
     EditText emailInput;
     EditText passwordInput;
     Button submitLoginButton;
+    MutableLiveData<Boolean> success=new MutableLiveData<>();
+    ProgressBar studentProgress;
+    AlertDialog alertDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +38,65 @@ public class LogInPage extends AppCompatActivity {
         passwordInput = (EditText) findViewById(R.id.editTextPasswordLogin);
 
         submitLoginButton = (Button) findViewById(R.id.btnLogin2);
+
+        studentProgress = (ProgressBar)findViewById(R.id.progressBar2);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Status of Action");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes",
+                new DialogInterface
+                        .OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog,
+                                        int which)
+                    {
+
+                        //change to different page
+                    }
+                });
+        alertDialog = builder.create();
+        final Observer<Boolean> obs = new Observer<Boolean>(){
+            @Override
+            public void onChanged(@Nullable final Boolean b){
+
+                if(b){
+                    studentProgress.setVisibility(View.GONE);
+                    alertDialog.setMessage("Succeeded in Logging In");
+                    alertDialog.show();
+
+
+
+
+                }
+                else{
+                    studentProgress.setVisibility(View.GONE);
+                    alertDialog.setMessage("Invalid Credentials");
+                    alertDialog.show();
+
+                }
+            }
+
+        };
+        success.observe(this, obs);
         submitLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 email = emailInput.getText().toString();
                 password = passwordInput.getText().toString();
+                LogInOut.LogIn(email,password,success);
+//                if(true) //do some backend checking here
+//                {
+//                    //go to next activity
+//                    showToast("Login verified and suceeded");
+//                }
+//                else if(false)
+//                {
+//                    showToast("You are not a registered user. Please sign up first");
+//                }
 
-                if(true) //do some backend checking here
-                {
-                    //go to next activity
-                    showToast("Login verified and suceeded");
-                }
-                else if(false)
-                {
-                    showToast("You are not a registered user. Please sign up first");
-                }
             }
         });
 
