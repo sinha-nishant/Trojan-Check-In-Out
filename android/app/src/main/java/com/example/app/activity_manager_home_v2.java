49f867bc85211902c7fd1bf80e216a29fca062a1
Markus Snelder <snelder@usc.edu>
2021-03-16 23:48:44 -0700

    made manager home fragment

---
+++ android/app/src/main/java/com/example/app/activity_manager_home_v2.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_manager_home_v2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_home_v2);
+    }
+
+
+}
