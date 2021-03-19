bdf1cde12f2e926a0a0164b7919905d1068d6af2
Markus Snelder <snelder@usc.edu>
2021-03-18 23:31:06 -0700

    navbar + made manager profile page

---
+++ android/app/src/main/java/com/example/app/ManagerProfile_v2.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerProfile_v2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_profile_v2);
+    }
+}
