24ae9161bdff1c1fa753005e9c2c1696f6395418
Angad Sood <angadsoo@usc.edu>
2021-03-24 06:27:18 +0530

    Testing for search

---
+++ android/app/src/androidTest/java/com/example/app/firebaseDB/FbQueryTest.java
@@ -0,0 +1,46 @@
+package com.example.app.firebaseDB;
+
+import android.content.Context;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.example.app.users.StudentAccount;
+import com.google.firebase.FirebaseApp;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FbQueryTest {
+    @Rule
+    public InstantTaskExecutorRule instantExecutorRule = new InstantTaskExecutorRule();
+
+    @Test
+    public void search() {
+        Context context = ApplicationProvider.getApplicationContext();
+        FirebaseApp.initializeApp(context);
+        Long uscID=8694251037L;
+        String emailExpected = "sancho@usc.edu";
+        MutableLiveData<StudentAccount> student = new MutableLiveData<>();
+        Observer<StudentAccount> saObserver = new Observer<StudentAccount>() {
+            @Override
+            public void onChanged(StudentAccount studentAccount) {
+                assertEquals(emailExpected, studentAccount.getEmail());
+            }
+        };
+        student.observeForever(saObserver);
+        FbQuery.search(uscID,student);
+        //To get the test to run add this - Firebase takes time to execute the query and the thread
+        //will just run in the background without testing the Firebase database if the code isn't
+        //there
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
