6fb4b505063eae1817c21a3e890eb283dcd436c5
Ahmad Rangeen <rangeen@usc.edu>
2021-03-24 11:32:57 -0700

    Update BuildingsOccupancyListTest.java

--- android/app/src/androidTest/java/com/example/app/blackBox/BuildingsOccupancyListTest.java
+++ android/app/src/androidTest/java/com/example/app/blackBox/BuildingsOccupancyListTest.java
@@ -25,6 +25,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.annotation.Nullable;
@@ -47,30 +49,39 @@ public class BuildingsOccupancyListTest {
     @Rule
     public InstantTaskExecutorRule instantExecutorRule = new InstantTaskExecutorRule();
 
+    @Rule
+    public ActivityTestRule<BuildingsOccupancyList> menuActivityTestRule = new ActivityTestRule<>(BuildingsOccupancyList.class);
+
+    public static List<Building>  post_value;
+
+    class Sortbyname implements Comparator<Building> {
+        // Used for sorting in ascending order of
+        // name
+        public int compare(Building a, Building b)
+        {
+            return a.getName().compareTo(b.getName());
+        }
+    }
 
 
     @Test
     public void onStart() {
         //get the card by finding buildingname using withChild(buildingname)
         //once we have card try withChild("occuapncy/cap")
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Context context = ApplicationProvider.getApplicationContext();
         FirebaseApp.initializeApp(context);
         MutableLiveData<List<Building>> builingMLD = new MutableLiveData<>();
 
         final Observer<List<Building>> observer = new Observer<List<Building>>(){
-
             @Override
             public void onChanged(@Nullable final List<Building> buildings){
-
-               for(int i=0;i<buildings.size();i++){
-                   String occupancymessage = "Occupancy: "+buildings.get(i).getOccupancy().toString()+"/100";
-                   onView(ViewMatchers.withId(R.id.recyclerList))
-                           .perform(RecyclerViewActions
-                                   .scrollTo(hasDescendant(withText(buildings.get(i).getName()))))
-                           .check(matches(hasDescendant(allOf(withText(buildings.get(i).getName()), hasSibling(withText(occupancymessage))))));
-
-               }
-
+                post_value=buildings;
             }
         };
         builingMLD.observeForever(observer);
@@ -80,6 +91,16 @@ public class BuildingsOccupancyListTest {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+        Collections.sort(post_value,new Sortbyname());
+
+        for(int i=0;i<post_value.size();i++){
+            String occupancymessage = "Occupancy: "+post_value.get(i).getOccupancy().toString()+"/100";
+            onView(ViewMatchers.withId(R.id.recyclerList))
+                    .perform(RecyclerViewActions
+                            .scrollTo(hasDescendant(withText(post_value.get(i).getName()))))
+                    .check(matches(hasDescendant(allOf(withText(post_value.get(i).getName()), hasSibling(withText(occupancymessage))))));
+
         }
 
 
