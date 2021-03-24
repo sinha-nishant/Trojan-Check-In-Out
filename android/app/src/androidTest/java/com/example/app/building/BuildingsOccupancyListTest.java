4c5a874f4bfec35b9625527b0fe3bed85eccf87d
Nishant Sinha <nishant813@gmail.com>
2021-03-23 22:18:26 -0700

    Think I fixed the match name and occupancy

--- android/app/src/androidTest/java/com/example/app/building/BuildingsOccupancyListTest.java
+++ android/app/src/androidTest/java/com/example/app/building/BuildingsOccupancyListTest.java
@@ -16,6 +16,7 @@ import androidx.test.rule.ActivityTestRule;
 import com.example.app.R;
 import com.example.app.account_UI.QRScan;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.google.android.material.card.MaterialCardView;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -27,12 +28,15 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.assertion.ViewAssertions.selectedDescendantsMatch;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withChild;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.JMock1Matchers.equalTo;
 import static org.hamcrest.Matchers.allOf;
@@ -57,42 +61,18 @@ public class BuildingsOccupancyListTest {
             e.printStackTrace();
 
         }
-//        onView(withId((R.id.recyclerList))).check(matches(hasSibling(withText("Annenberg House"))));
+
+//        onView(ViewMatchers.withId(R.id.recyclerList))
+//                // scrollTo will fail the test if no item matches.
+//                .perform(RecyclerViewActions.scrollTo(
+//                        hasDescendant(allOf(withText("Annenberg House"),
+//                                hasSibling(withText(containsString("Occupancy: 88/100"))),
+//                                isDisplayed()))
+//        ));
 
         onView(ViewMatchers.withId(R.id.recyclerList))
-                // scrollTo will fail the test if no item matches.
-                .perform(RecyclerViewActions.scrollTo(
-                        hasDescendant(allOf(withText("Annenberg House"),
-                                hasSibling(withText(containsString("Occupancy: 88/100"))),
-                                isDisplayed()))
-                ));
-//        onView(withId(R.id.recyclerList)).
-//        RecyclerView rv = activityRule.getActivity().findViewById(R.id.recyclerList);
-//        int item_count = rv.getAdapter().getItemCount();
-//        onView(withId(R.id.recyclerList)).perform(RecyclerViewActions.scrollToPosition(item_count-1));
-//         Log.d(rv.getLayoutManager().getChildAt(1).findViewById(R.id.buildingName).getTag().toString(),"HEREIAM");
-//        onView(withId(R.id.buildingName)).perform(scrollTo());
-//
-//        onView(withId(R.id.recyclerList)).perform(actionOnItemAtPosition(50,scrollTo()));
-        try{Thread.sleep(5000);}
-
-        catch (Exception e){
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                .perform(RecyclerViewActions
+                .scrollTo(hasDescendant(withText("Annenberg House"))))
+                .check(matches(hasDescendant(allOf(withText("Annenberg House"), hasSibling(withText("Occupancy: 89/100"))))));
     }
 }
