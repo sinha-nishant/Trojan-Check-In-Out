4f7242b805c359ede210ff69b1d3b04b1e33e1c4
Ahmad Rangeen <rangeen@usc.edu>
2021-03-30 14:57:01 -0700

    disabled button until file is uploaded

--- android/app/src/main/java/com/example/app/account_UI/ManagerCSV.java
+++ android/app/src/main/java/com/example/app/account_UI/ManagerCSV.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.app.R;
 
@@ -15,11 +16,23 @@ import java.io.File;
 public class ManagerCSV extends AppCompatActivity {
     private Integer readRequestCode = 123;
     private Button upload;
+    private TextView fileName;
+    private String fileNamePrefix = "File Chosen:";
+    private Button confirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_c_s_v);
         upload = findViewById(R.id.chooseFileBtn);
+        fileName = findViewById(R.id.fileNameTextView);
+        confirm = findViewById(R.id.confirmBtn);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Update","Btn pressed");
+            }
+        });
+        confirm.setEnabled(false);
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +53,16 @@ public class ManagerCSV extends AppCompatActivity {
         if (resultCode == RESULT_OK){
             File csvFile = new File(data.getData().getPath());
              Log.d("Got CSV",csvFile.toString());
+             confirm.setEnabled(true);
+             String fileChosen = fileNamePrefix+csvFile.getName();
+             fileName.setText(fileChosen);
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        confirm.setEnabled(false);
+        fileName.setText("");
     }
 }
