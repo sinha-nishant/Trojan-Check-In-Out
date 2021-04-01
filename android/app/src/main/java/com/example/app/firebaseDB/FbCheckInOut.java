30200f25d9e2ba982dc2aef26b644334139f9a1c
Nishant Sinha <nishant813@gmail.com>
2021-03-31 18:02:07 -0700

    Updated check in and check out to work with new activities collection

--- android/app/src/main/java/com/example/app/firebaseDB/FbCheckInOut.java
+++ android/app/src/main/java/com/example/app/firebaseDB/FbCheckInOut.java
@@ -11,83 +11,80 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.WriteBatch;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FbCheckInOut implements FirestoreConnector {
-    // Arjun: Updated paramaters to allow for callback
     public static void checkIn(Long uscID, StudentActivity sa, MutableLiveData<Boolean> success) {
-        CollectionReference accounts = FirestoreConnector.getDB().collection("Accounts");
-        Query studentQuery = accounts.whereEqualTo("uscID", uscID);
+        Query studentQuery = FirestoreConnector.getDB().collection("Accounts").whereEqualTo("uscID", uscID);
         studentQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    if (!task.getResult().isEmpty()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            accounts.document(document.getId()).update("activity", FieldValue.arrayUnion(sa));
-                        }
+                if (task.isSuccessful() && !task.getResult().isEmpty()) {
+                    FirebaseFirestore db = FirestoreConnector.getDB();
+                    WriteBatch batch = db.batch();
+                    batch.update(task.getResult().getDocuments().get(0).getReference(), "activity", FieldValue.arrayUnion(sa));
+                    // Convert object to hashmap so can add 'uscID' field as foreign key
+                    HashMap<String, Object> studentActivity = new HashMap<>();
+                    studentActivity.put("buildingName", sa.getBuildingName());
+                    studentActivity.put("checkInTime", sa.getCheckInTime());
+                    studentActivity.put("checkOutTime", sa.getCheckOutTime());
+                    studentActivity.put("uscID", uscID);
+                    batch.set(db.collection("Activities").document(), studentActivity);
 
-                        CollectionReference buildings = FirestoreConnector.getDB().collection("Buildings");
-                        Query buildingQuery = buildings.whereEqualTo("name", sa.getBuildingName());
-                        buildingQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    if (!task.getResult().isEmpty()) {
-                                        Map<String, Object> updates = new HashMap<>();
-                                        updates.put("occupancy", FieldValue.increment(1));
-                                        updates.put("students_ids", FieldValue.arrayUnion(uscID));
-                                        for (QueryDocumentSnapshot document : task.getResult()) {
-                                            buildings.document(document.getId())
-                                                    .update(updates);
+                    Query buildingQuery = FirestoreConnector.getDB().collection("Buildings").whereEqualTo("name", sa.getBuildingName());
+                    buildingQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful() && !task.getResult().isEmpty()) {
+                                Map<String, Object> updates = new HashMap<>();
+                                updates.put("occupancy", FieldValue.increment(1));
+                                updates.put("students_ids", FieldValue.arrayUnion(uscID));
+                                batch.update(task.getResult().getDocuments().get(0).getReference(), updates);
+                                batch.commit().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (!task.isSuccessful()) Log.d("CHECKIN", String.valueOf(task.getException()));
+                                            success.setValue(task.isSuccessful());
                                         }
-
-                                        // call callback function
-                                        success.setValue(true);
-
                                     }
-                                } else {
-                                    success.setValue(false);
-                                }
+                                );
+                            } else {
+                                Log.d("CHECKIN", String.valueOf(task.getException()));
+                                success.setValue(false);
                             }
-                        });
-                    }
+                        }
+                    });
+                } else {
+                    Log.d("CHECKIN", String.valueOf(task.getException()));
+                    success.setValue(false);
                 }
-
             }
         });
     }
 
     public static void checkOut(Long uscID, StudentActivity sa, Date checkOutTime, MutableLiveData<Boolean> success) {
-        FirestoreConnector.getDB().collection("Accounts")
-                .whereEqualTo("uscID", uscID)
-                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        FirestoreConnector.getDB().collection("Accounts").whereEqualTo("uscID", uscID).get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful() && !task.getResult().isEmpty()) {
                     DocumentReference account = task.getResult().getDocuments().get(0).getReference();
-
-                    StudentActivity newSA = new StudentActivity(
-                            sa.getBuildingName(),
-                            sa.getCheckInTime(),
-                            checkOutTime
-                    );
-                    account.update(
-                            "activity", FieldValue.arrayRemove(sa),
-                            "activity", FieldValue.arrayUnion(newSA))
+                    StudentActivity newSA = new StudentActivity(sa.getBuildingName(), sa.getCheckInTime(), checkOutTime);
+                    account.update("activity", FieldValue.arrayRemove(sa),"activity", FieldValue.arrayUnion(newSA))
                             .addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()) {
                                         FirestoreConnector.getDB().collection("Buildings")
-                                                .whereEqualTo("name", sa.getBuildingName())
-                                                .get()
+                                                .whereEqualTo("name", sa.getBuildingName()).get()
                                                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                                                     @Override
                                                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -100,12 +97,36 @@ public class FbCheckInOut implements FirestoreConnector {
                                                                 @Override
                                                                 public void onComplete(@NonNull Task<Void> task) {
                                                                     if (task.isSuccessful()) {
-                                                                        Log.d("CHECKOUT", "checked out successfully!");
+                                                                        FirestoreConnector.getDB().collection("Activities")
+                                                                                .whereEqualTo("uscID", uscID)
+                                                                                .whereEqualTo("checkOutTime", null)
+                                                                                .get()
+                                                                                .addOnCompleteListener(
+                                                                                        new OnCompleteListener<QuerySnapshot>() {
+                                                                                            @Override
+                                                                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                                                                if (task.isSuccessful() && !task.getResult().isEmpty()) {
+                                                                                                    task.getResult().getDocuments().get(0).getReference()
+                                                                                                            .update("checkOutTime", checkOutTime)
+                                                                                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                                                @Override
+                                                                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                                                                    if (!task.isSuccessful()) Log.d("CHECKOUT", String.valueOf(task.getException()));
+                                                                                                                    success.setValue(task.isSuccessful());
+                                                                                                                }
+                                                                                                            });
+                                                                                                }
 
-                                                                        //Arjun added callback
-                                                                        success.setValue(true);
+                                                                                                else {
+                                                                                                    Log.d("CHECKOUT", String.valueOf(task.getException()));
+                                                                                                    success.setValue(false);
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                );
                                                                     } else {
                                                                         //Arjun added callback
+                                                                        Log.d("CHECKOUT", String.valueOf(task.getException()));
                                                                         success.setValue(false);
                                                                     }
                                                                 }
