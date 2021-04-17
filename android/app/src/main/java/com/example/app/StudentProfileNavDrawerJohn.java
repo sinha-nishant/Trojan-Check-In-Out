8adc0d54fa5e4dc6d87320d583f13d6088afff47
John Paine <johnpain@usc.edu>
2021-04-17 13:45:13 -0700

    More Nav Drawer Functionality

--- android/app/src/main/java/com/example/app/StudentProfileNavDrawerJohn.java
+++ android/app/src/main/java/com/example/app/StudentProfileNavDrawerJohn.java
@@ -1,21 +1,64 @@
 package com.example.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class StudentProfileNavDrawerJohn extends AppCompatActivity {
+import com.example.app.account_UI.StudentHistory;
+import com.google.android.material.navigation.NavigationView;
+
+public class StudentProfileNavDrawerJohn extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private TextView mTextView;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_profile_nav_drawer_john);
+        NavigationView nv = findViewById(R.id.nav_view);
+        nv.setNavigationItemSelectedListener(this);
+        /*
+        Toolbar tb = findViewById(R.id.toolbar2);
+        setSupportActionBar(tb);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        drawer = findViewById(R.id.drawer_layout2);
 
-        // Enables Always-on
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, tb,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        */
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.nav_profile_message:
+                Intent i = new Intent(this, StudentHistory.class);
+                startActivity(i);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
